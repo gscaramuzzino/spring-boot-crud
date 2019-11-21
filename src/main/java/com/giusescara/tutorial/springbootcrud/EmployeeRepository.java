f7383d238dade3c418996b8f2ab5a8d67e122dbb
Giuseppe Scaramuzzino <giuseppe.scaramuzzino@experian-ma.com>
2019-11-21 11:02:38 +0100

    Add EmployeeEntity and JpaRepository

---
+++ src/main/java/com/giusescara/tutorial/springbootcrud/EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.giusescara.tutorial.springbootcrud;
+
+import com.giusescara.tutorial.springbootcrud.model.EmployeeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
+}
